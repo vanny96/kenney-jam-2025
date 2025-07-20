fdd94a779182b8371a1ed4a8c3b6dcaa86cdba03
Giovanni Barbaro <vanni-96@hotmail.it>
2025-07-20 13:55:40 +0200

    Fixes endmenu button

--- Scenes/Menus/EndScreen/end_screen.gd
+++ Scenes/Menus/EndScreen/end_screen.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 @export var return_to_menu_button: Button 
 
-@onready var main_menu: PackedScene = preload("res://Scenes/Menus/MainMenu/MainMenu.tscn")
+@onready var main_menu_path = "res://Scenes/Menus/MainMenu/MainMenu.tscn"
 
 func _ready() -> void:
 	MenuAudio.configure_button(return_to_menu_button)
 	return_to_menu_button.pressed.connect(return_to_menu)
 	
 func return_to_menu():
-	SceneManager.change_scene_to_packed(main_menu)
+	SceneManager.change_scene(main_menu_path)
