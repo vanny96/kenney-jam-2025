891051e77d1b3fc00933756aa9d3137ac9103efd
Giovanni Barbaro <vanni-96@hotmail.it>
2025-07-20 08:35:04 +0200

    Adds sleeping zoom

--- Scenes/Characters/Heroine/StateMachine/sleep.gd
+++ Scenes/Characters/Heroine/StateMachine/sleep.gd
@@ -10,8 +10,10 @@ func _enter() -> void:
 	animation_player.play("heroine/sleep")
 	sleeping_vfs.emitting = true
 	sleeping_vfs.color = Color.WHITE
+	GlobalSignals.sleeping_started.emit()
 	heroine.model.position.y = 0.5
 	passed_time = 0
+	
 	
 func _update(delta: float) -> void:
 	passed_time += delta
@@ -24,4 +26,5 @@ func _exit() -> void:
 	heroine.model.position.y = 0
 	sleeping_vfs.color = Color(Color.WHITE, 0)
 	sleeping_vfs.emitting = false
+	GlobalSignals.sleeping_ended.emit()
 	
--- Scenes/Levels/Sandbox/camera_3d.gd
+++ Scenes/Levels/Sandbox/camera_3d.gd
@@ -1,8 +1,32 @@
 extends Camera3D
 
 @export var target: Node3D
-@export var offset: Vector3
+
+@export var initial_offset: Vector3
+@onready var initial_rotation: Vector3 = rotation_degrees
+
+@export var sleeping_offset: Vector3
+@export var sleeping_rotation: Vector3
+
+@export var transition_speed: float
+
+@onready var target_offset = initial_offset
+@onready var target_rotation = initial_rotation
+
+var target_tween: Tween
+
+func _ready() -> void:
+	GlobalSignals.sleeping_started.connect(update_targets.bind(sleeping_offset, sleeping_rotation))
+	GlobalSignals.sleeping_ended.connect(update_targets.bind(initial_offset, initial_rotation))
+
 
 func _process(delta: float) -> void:
 	if target:
-		position = target.position - offset
+		position = target.position - target_offset
+		rotation_degrees = target_rotation
+
+func update_targets(p_offset: Vector3, p_rotation: Vector3):
+	target_tween = create_tween()
+	target_tween.parallel().tween_property(self, "target_offset", p_offset, transition_speed)
+	target_tween.parallel().tween_property(self, "target_rotation", p_rotation, transition_speed)
+	
